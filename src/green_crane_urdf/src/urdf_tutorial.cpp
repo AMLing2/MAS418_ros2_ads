e7e2ac7e21ad3f37846dfc9349e35bd45166dd44
AMLing2 <mathias.lervik@hotmail.com>
2025-12-05 10:10:39 +0100

    start of ads and ros2 node

--- src/green_crane_urdf/src/urdf_tutorial.cpp
+++ src/green_crane_urdf/src/urdf_tutorial.cpp
@@ -2,8 +2,10 @@
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/joint_state.hpp>
 #include <cmath>
-#include <thread>
-#include <chrono>
+#include <unistd.h>
+#include "AdsLib.h"
+#include "AdsVariable.h"
+#include "standalone/AdsDef.h"
 
 using namespace std::chrono;
 
@@ -58,9 +60,58 @@ void StatePublisher::publish(){
     RCLCPP_INFO(this->get_logger(),"Publishing joint state");
 }
 
+struct AdsVariables
+{
+	AdsVariables() = delete;
+
+	explicit AdsVariables(AdsDevice& route)
+	: craneAngle{route, "MAIN.bActivateMotion"}
+	// , velocityReference{route, "MAIN.fVelRef"}
+	// , positionReference{route, "MAIN.fPosRef"}
+	// , positionMeasurement{route, "MAIN.fPosMeas"}
+	{
+		// Do nothing.
+	}
+
+	AdsVariable<double> craneAngle;
+};
+
+class AdsComm{
+public:
+	explicit AdsComm(const AmsNetId remoteNetId, const std::string remoteIpV4)
+	:remoteNetId_(remoteNetId)
+	,remoteIpV4_(remoteIpV4)
+	,route_(remoteIpV4_,remoteNetId_,AMSPORT_R0_PLC_TC3)
+	,ads_(route_)
+	{}
+	
+	double getCraneAngle(){
+		return ads_.craneAngle;
+	}
+	void printState(){
+		const auto state = route_.GetState();
+		std::cout << "ADS state: "
+							<< std::dec << static_cast<uint16_t>(state.ads)
+							<< " devState: "
+							<< std::dec << static_cast<uint16_t>(state.device);
+	}
+private:
+	const AmsNetId remoteNetId_;
+	const std::string remoteIpV4_;
+	AdsDevice route_;
+	AdsVariables ads_;
+};
+
+
 int main(int argc, char * argv[]){
-    rclcpp::init(argc,argv);
-    rclcpp::spin(std::make_shared<StatePublisher>());
-    rclcpp::shutdown();
-    return 0;
+	rclcpp::init(argc,argv);
+	const AmsNetId remoteNetId {192,168,0,10,1,1};
+	const std::string remoteIpV4 = "192.168.0.10";
+	AdsComm adsComm(remoteNetId,remoteIpV4);
+	adsComm.printState();
+	std::cout<<"angle:"<<adsComm.getCraneAngle()<<std::endl;
+	return 0;
+	rclcpp::spin(std::make_shared<StatePublisher>());
+	rclcpp::shutdown();
+	return 0;
 }
